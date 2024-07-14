4125deecc47cb9583aeaa2b98d16b4f251ab2ae5
prajjal-saxena <prajjalsaxena@gmail.com>
2024-07-14 23:07:36 +0530

    first commit

--- src/features/product-list/productListAPI.js
+++ src/features/product-list/productListAPI.js
@@ -1,5 +1,5 @@
 // A mock function to mimic making an async request for data
-export function fetchCount(amount = 1) {
+export default function fetchCount(amount = 1) {
   return new Promise(async(resolve) =>{
      const response =  await fetch('http://localhost:8080')
      const data = await response.json();

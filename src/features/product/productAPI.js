9b16d1d7c0489f4771ec5636d7f09759c90f0d87
unknown <prajjalsaxena@gmail.com>
2024-08-02 18:01:47 +0530

    jsonserver dummy

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -1,7 +1,7 @@
 // A mock function to mimic making an async request for data
-export default function fetchCount(amount = 1) {
+export default function fetchAllProducts() {
   return new Promise(async(resolve) =>{
-     const response =  await fetch('http://localhost:8080')
+     const response =  await fetch('http://localhost:8080/products')
      const data = await response.json();
      resolve({data})}
   );
